4da2b74edf641324a4ffbb944927e469a4b37f3c
Helen <Lena_b89@mail.ru>
2025-08-21 23:21:51 +0300

    автотетс на первые три поля

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,16 @@
 import pytest
 from selene import browser
+from selenium import webdriver
 
 
 @pytest.fixture(scope="function", autouse=True)
 def browser_managment():
     browser.config.base_url = "https://demoqa.com/automation-practice-form"
+    driver_options = webdriver.ChromeOptions()
+    driver_options. add_argument("--headless")
+    #browser.config.timeout = 3.0
+    browser.config.driver_options = driver_options
+
 
     yield
 
--- tests/test_demoga.py
+++ tests/test_demoga.py
@@ -1,9 +1,11 @@
 from selene import browser, have, be
 
 
-def test_complete_todo():
+def test_correct_completion():
 
     browser. open("/")
     browser.element("#firstName"). should(be. blank)
-
-    browser.element("#firstName").type("Test").press_enter()
+    browser.element("#firstName").type("Test")
+    browser.element("#lastName").should(be.blank)
+    browser.element("#lastName").type("Testov")
+    browser.element("#userEmail").type("Test@mail.ru")
